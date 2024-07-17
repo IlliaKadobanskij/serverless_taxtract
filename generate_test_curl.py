e874df85dc4f85d9a8b2b3a003a49df3c07b9c19
KadobiankijI <55656959+IlliaKadobanskij@users.noreply.github.com>
2024-07-17 16:19:56 +0300

    Update generate_test_curl.py

--- generate_test_curl.py
+++ generate_test_curl.py
@@ -4,7 +4,7 @@ file_path = 'textextract-bad1.pdf'
 with open(file_path, 'rb') as file:
     encoded_file = base64.b64encode(file.read()).decode('utf-8')
 
-curl_command = f'curl -X POST "https://4d3t3csb80.execute-api.us-east-1.amazonaws.com/dev/files" \
+curl_command = f'curl -X POST "https://owu9p0i146.execute-api.us-east-1.amazonaws.com/dev/files" \
     -H "Content-Type: application/json" \
     -d \'{{"file": "{encoded_file}"}}\''
 
